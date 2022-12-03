f6b286ae64c96b565b80f59106d3dfae8ebf59e3
alexander-ra <alexander.andreev37@gmail.com>
2022-12-03 10:02:31 +0200

    Add responsive design for content pages for landscape

--- src/components/contentPage/TextSection.tsx
+++ src/components/contentPage/TextSection.tsx
@@ -4,6 +4,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import {ContentData, ContentLabels} from "../../labels/ContentLabels";
 import Wings from "./Wings";
+import {current} from "@reduxjs/toolkit";
+import Utils from "../../utils/Utils";
 
 export enum TextSectionPosition {
     LEFT = "LEFT",
@@ -17,9 +19,33 @@ interface ContentBubbleProps {
 
 
 class TextSection extends React.Component<ContentBubbleProps> {
+    private myRef: React.RefObject<HTMLDivElement>;
 
     constructor(props: ContentBubbleProps) {
         super(props);
+        this.myRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.updateDimensions();
+        window.addEventListener('resize', this.updateDimensions);
+    }
+
+    componentDidUpdate(prevProps: Readonly<ContentBubbleProps>, prevState: Readonly<{}>, snapshot?: any) {
+        setTimeout(() => {
+            if (prevProps.data.description !== this.props.data.description) {
+                this.updateDimensions();
+            }
+        });
+    }
+
+    private updateDimensions = () => {
+        const currElement = this.myRef.current;
+        if (Utils.isNotNull(currElement?.style)) {
+            const parentElement = currElement?.parentElement;
+            const parentHeightDiff = parentElement?.offsetHeight - currElement?.offsetHeight;
+            currElement.style.margin = `${parentHeightDiff / 2}px 0`;
+        }
     }
 
     render(){
@@ -27,14 +53,14 @@ class TextSection extends React.Component<ContentBubbleProps> {
         return (
             <div className={`text-section text-section-${this.props.sectionPosition.toLowerCase()}`}>
                 <div className={"content"}>
-                    <div className={"content-title"}>
-                        {data.title}
-                    </div>
-                    <div className={"content-body"}>
+                    <div className={"content-title"}>{data.title}</div>
+                    <div className="mask"></div>
+                    <div className="mask-dve"></div>
+                    <div className={"content-text"} ref={this.myRef}>
+                        {/*<div className={"content-title"}>{data.title}</div>*/}
                         {data.description}
                     </div>
                 </div>
-                <Wings sectionPosition={this.props.sectionPosition} />
             </div>
         )
     }
