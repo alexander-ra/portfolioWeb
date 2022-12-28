3d417e90e97785263434e1febe5f3954691358cf
alexander-ra <alexander.andreev37@gmail.com>
2022-12-28 18:20:46 +0200

    Add contact links

--- src/components/layout/ContactsSubMenu.tsx
+++ src/components/layout/ContactsSubMenu.tsx
@@ -23,23 +23,44 @@ class NavigationSubMenu extends React.Component<ExpandedMenuProps, ExpandedMenuS
                 <div className={"contact-right"}>
                     <div className={"contact-line"}>
                         <Icon className={"contact-icon"} icon={IconType.faMobile} />
-                        <div className={"expanded-menu-label"}>(+359) 885-654-188</div>
+                        <div className={"expanded-menu-label"}>
+                            <a href="tel:+359-654-188">
+                                (+359) 885-654-188
+                            </a>
+                        </div>
                     </div>
                     <div className={"contact-line"}>
                         <Icon className={"contact-icon"} icon={IconType.faEnvelope} />
-                        <div className={"expanded-menu-label"}>info@alexdev.pro</div>
+                        <div className={"expanded-menu-label"}>
+                            <a href="mailto:alexander.andreev37@gmail.com">
+                                info@alexdev.pro
+                            </a>
+                        </div>
                     </div>
                     <div className={"contact-line"}>
                         <Icon className={"contact-icon"} icon={IconType.faLinkedin} />
-                        <div className={"expanded-menu-label"}>/in/alexandar-andreev</div>
+                        <div className={"expanded-menu-label"}>
+                            <a href="https://www.linkedin.com/in/alexandar-andreev/" target="_blank">
+                                /in/alexandar-andreev
+                            </a>
+                        </div>
                     </div>
                     <div className={"contact-line"}>
                         <Icon className={"contact-icon"} icon={IconType.faGithubAlt} />
-                        <div className={"expanded-menu-label"}>alexander-ra/portfolioWeb</div>
+                        <div className={"expanded-menu-label"}>
+                            <a href="https://www.google.com/" target="_blank">
+                                alexander-ra/portfolioWeb
+                            </a>
+                        </div>
                     </div>
                     <div className={"contact-line"}>
                         <Icon className={"contact-icon"} icon={IconType.faSave} />
-                        <div className={"expanded-menu-label"}>Download Resume.pdf</div>
+                        <div className={"expanded-menu-label"}>
+                            <a href="http://192.168.8.30:3000/static/media/avatar-jpeg.fbeb0d852a7c4716f705.jpg"
+                               target="_blank" download="image.jpg">
+                                Download Resume.pdf
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
